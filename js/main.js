9ae2a578c64ab9e8e935ed5b060983deb6b93362
Denis Isakov <denisakov@gmail.com>
2018-02-20 02:16:45 +0300

    qqsas

--- js/main.js
+++ js/main.js
@@ -51,5 +51,69 @@ $(document).on('click', '.delete-product', function () {
     // currentProduct.remove();
     return true;
 });
-$.watermarker.setDefaults({ color: '#f00', left: 8 });
-$('input#comment').watermark();
+// Bind to the submit event of our form
+function formSubmit(event) {
+
+	// Abort any pending request
+	if (request) {
+		request.abort();
+	}
+	// setup some local variables
+	var $form = $('#order');
+
+	// Let's select and cache all the fields
+	var $inputs = $form.find("input, select, button, textarea");
+
+	// Serialize the data in the form
+	var serializedData = $form.serialize();
+
+	// Let's disable the inputs for the duration of the Ajax request.
+	// Note: we disable elements AFTER the form data has been serialized.
+	// Disabled form elements will not be serialized.
+	$inputs.prop("disabled", true);
+
+	// Fire off the request to /form.php
+	request = $.ajax({
+		url: "https://script.google.com/a/macros/userconversion.com/s/AKfycbw3Sp6Ivvh0ROz9HO_VFfuiis0KnfsNQPQtwhi3HtcLNV_emGdJ/exec",
+		method: "POST",
+		dataType: 'jsonp',
+		data: serializedData
+	});
+
+	// Callback handler that will be called on success
+	request.done(function (response, textStatus, jqXHR) {
+		// Log a message to the console
+	});
+
+	// Callback handler that will be called on failure
+	request.fail(function (jqXHR, textStatus, errorThrown) {
+		// Log the error to the console
+
+	});
+
+	// Callback handler that will be called regardless
+	// if the request failed or succeeded
+	request.always(function (response) {
+		// Reenable the inputs
+		$inputs.prop("disabled", false);
+		console.log(response.result + ". Row " + response.row + " was created.");
+	});
+
+	// Prevent default posting of form
+	if (event) event.preventDefault();
+};
+function clearForm() {
+	$('#order')[0].reset();
+}
+$('#order').submit(function (e) {
+	e.preventDefault();
+	//console.log('The button is clicked');
+	// update hidden inputs
+	formSubmit();
+	setTimeout(function () {
+		// var r = confirm("Form Submitted. You can now close the window or select \"OK\" to create a new form");
+		// if (r == true) {
+		clearForm();
+		// }
+	}, 500)
+});
